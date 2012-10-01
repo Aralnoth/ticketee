f6ff71e6352f909dcf2e17bc11b9f8ff03802d91
Aralnoth <jose@jose-valeria.(none)>
2012-10-01 13:24:23 +0200

    Redirect the users back to the projects page if they try going to a project that doesn't exist.

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe ProjectsController do
-
+	it "display an error for a missing project" do 
+		get :show, :id =>"not-here"
+		response.should redirect_to(project_path)
+		message = "The project you were looking for could not be found."
+		flash[:alert].should == message
+	end
 end
